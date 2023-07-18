59de49d830733b96453cac616d0822aeb23921f5
Manuel Vogel <ge87xig@mytum.de>
2023-07-18 12:54:35 +0200

    first sucessful test on real robot

--- safety_shield/src/testnode.cc
+++ safety_shield/src/testnode.cc
@@ -19,15 +19,6 @@ TestNode::TestNode(const Args& args)
 bool TestNode::on_initialize()
 {
     //init ROS
-    int argc = 0;
-    char **argv = NULL;
-    ros::init(argc, argv, "testnode");
-    return true;
-}
-
-
-void TestNode::on_start()
-{
     // ros: subsribe to topics and advertise topics
     ros::NodeHandle nh;
     _model_state_sub = nh.subscribe("/gazebo/model_states", 100, &TestNode::modelStatesCallback, this);
@@ -39,7 +30,8 @@ void TestNode::on_start()
     
     // we must explicitly set the control mode for our robot
     // in this case, we will only send positions
-    _robot->setControlMode(ControlMode::Position() + ControlMode::Velocity());
+    setDefaultControlMode(ControlMode::Position() + ControlMode::Velocity());
+    // _robot->setControlMode(ControlMode::Position() + ControlMode::Velocity());
     
     // do some on-start initialization
     _robot->sense();
@@ -49,15 +41,19 @@ void TestNode::on_start()
     bool activate_shield = true;
     _st_time = chrono::steady_clock::now();
 
-    double sample_time = 0.001;
+    double sample_time = getPeriodSec();
     //Note: executing directory is /tmp/something/ TODO: make path relative somehow
     //std::string trajectory_config_file = std::string("/home/user/concert_ws/src/sara-shield/safety_shield/config/trajectory_parameters_schunk.yaml");
     //std::string robot_config_file = std::string("/home/user/concert_ws/src/sara-shield/safety_shield/config/robot_parameters_schunk.yaml");
     //std::string mocap_config_file = std::string("/home/user/concert_ws/src/sara-shield/safety_shield/config/cmu_mocap_no_hand.yaml");
-    std::string trajectory_config_file = std::string(std::getenv("HOME")) + "/concert_ws/src/concert_description/sara-shield/safety_shield/config/trajectory_parameters_concert.yaml";
-    std::string robot_config_file = std::string(std::getenv("HOME")) + "/concert_ws/src/concert_description/sara-shield/safety_shield/config/robot_parameters_concert.yaml";
-    std::string mocap_config_file = std::string(std::getenv("HOME")) + "/concert_ws/src/concert_description/sara-shield/safety_shield/config/cmu_mocap_no_hand.yaml";
+    // std::string trajectory_config_file = std::string(std::getenv("HOME")) + "/concert_ws/src/concert_description/sara-shield/safety_shield/config/trajectory_parameters_concert.yaml";
+    // std::string robot_config_file = std::string(std::getenv("HOME")) + "/concert_ws/src/concert_description/sara-shield/safety_shield/config/robot_parameters_concert.yaml";
+    // std::string mocap_config_file = std::string(std::getenv("HOME")) + "/concert_ws/src/concert_description/sara-shield/safety_shield/config/cmu_mocap_no_hand.yaml";
     //std::string robot_config_file = std::string("/tmp/robot_parameters_schunk.yaml");
+
+    std::string trajectory_config_file = getParamOrThrow<std::string>("~trajectory_config_file");
+    std::string robot_config_file = getParamOrThrow<std::string>("~robot_config_file");
+    std::string mocap_config_file = getParamOrThrow<std::string>("~mocap_config_file");
 
     double init_x = 0.0;
     double init_y = 0.0;
@@ -90,13 +86,27 @@ void TestNode::on_start()
       _human_meas[i] = reach_lib::Point(400.0, 400.0, 0.0);
     }
 
+  return true;
 
     //auto start_time = std::chrono::system_clock::now();
     //double t = std::chrono::duration<double>(std::chrono::system_clock::now()-start_time).count();
     //spdlog::info("Debug started.");
+}
 
-    // we can switch to run
-    start_completed();
+
+void TestNode::on_start()
+{
+    _robot->sense();
+    
+    Eigen::VectorXd init_qpos_arm_eigen;
+    _robot->chain("chain_E").getJointPosition(init_qpos_arm_eigen);
+
+    std::cout << "starting from q = " << init_qpos_arm_eigen.transpose() << "\n";
+
+    std::vector<double> init_qpos_arm(init_qpos_arm_eigen.data(),
+                                      init_qpos_arm_eigen.data() + init_qpos_arm_eigen.size());
+
+    _shield.reset(true, 0, 0, 0, 0, 0, 0, init_qpos_arm, ros::Time::now().toSec());
 
 }
 
